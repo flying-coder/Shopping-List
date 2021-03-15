bfdc26e38a25e28e6f62e1eb6d6a28e5e578e758
flying-coder <mayashinagawa@gmail.com>
2021-03-15 16:44:43 +0900

    test

--- server/index.js
+++ server/index.js
@@ -10,13 +10,27 @@ app.use(cors());
 app.use(express.json());
 
 //
-//ROUTE
+//ROUTE-
 //
 
 //create shopping list
+app.post("/shoppinglist", async (req, res) => {
+    try {
+        const { description } = req.body;
+        const newSL = await pool.query(
+            "INSERT INTO shoppinglist (description) ",
+            [des]
+        );
+
+    } catch (error) {
+        console.error(error.message);
+
+    }
+});
 
 
 
+//call server
 app.listen(5000, () => {
-    console.log("server has started on port 5000")
-})
+    console.log("server has started on port 5000");
+});
